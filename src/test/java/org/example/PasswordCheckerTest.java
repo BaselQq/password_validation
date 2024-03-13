c2a44e40f373683aa87f9e92c232d091839208cc
BaselQq <73541125+BaselQq@users.noreply.github.com>
2024-03-13 12:37:10 +0100

    - use ParameterizedTest

--- src/test/java/org/example/PasswordCheckerTest.java
+++ src/test/java/org/example/PasswordCheckerTest.java
@@ -2,101 +2,111 @@ package org.example;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordCheckerTest {
 
-    @Test
-    void checkPasswordLength_shouldReturnFalse_whenPasswordLengthLessThanEight() {
-        // GIVEN
-        String password = "a123456";
-        // WHEN
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                "a123456"
+            }
+    )
+    void checkPasswordLength_shouldReturnFalse_whenPasswordLengthLessThanEight(String password) {
         boolean actual = PasswordChecker.checkPasswordLength(password);
-        // THEN
         Assertions.assertFalse(actual);
     }
 
-    @Test
-    void checkPasswordLength_shouldReturnTrue_whenPasswordLengthMoreThanEight() {
-        // GIVEN
-        String password = "123456789";
-        // WHEN
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                "123456789"
+            }
+    )
+    void checkPasswordLength_shouldReturnTrue_whenPasswordLengthMoreThanEight(String password) {
         boolean actual = PasswordChecker.checkPasswordLength(password);
-        // THEN
         Assertions.assertTrue(actual);
     }
 
-    @Test
-    void checkPasswordContainsDigit_shouldReturnTrue_whenPasswordContainsAtLeastOneDigit() {
-        // GIVEN
-        String password = "1asdA";
-        // WHEN
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "1asdA"
+            }
+    )
+    void checkPasswordContainsDigit_shouldReturnTrue_whenPasswordContainsAtLeastOneDigit(String password) {
         boolean actual = PasswordChecker.checkPasswordContainsDigit(password);
-        // THEN
         Assertions.assertTrue(actual);
     }
 
-    @Test
-    void checkPasswordContainsDigit_shouldReturnFalse_whenPasswordDoesntContainDigits() {
-        // GIVEN
-        String password = "sadkfjA";
-        // WHEN
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "sadkfjA"
+            }
+    )
+    void checkPasswordContainsDigit_shouldReturnFalse_whenPasswordDoesntContainDigits(String password) {
         boolean actual = PasswordChecker.checkPasswordContainsDigit(password);
-        // THEN
         Assertions.assertFalse(actual);
     }
 
-    @Test
-    void checkPasswordUpperAndLowerCase_shouldReturnTrue_whenPasswordContainsBothUpperAndLowerCaseLetters() {
-        // GIVEN
-        String password = "Asdft1";
-        // WHEN
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "Asdft1"
+            }
+    )
+    void checkPasswordUpperAndLowerCase_shouldReturnTrue_whenPasswordContainsBothUpperAndLowerCaseLetters(String password) {
         boolean actual = PasswordChecker.checkPasswordUpperAndLowerCase(password);
-        // THEN
         Assertions.assertTrue(actual);
     }
 
-    @Test
-    void checkPasswordUpperAndLowerCase_shouldReturnFalse_whenPasswordDoesntContainOnlyUpperCaseLetters() {
-        // GIVEN
-        String passwrod = "ASDASGAQW";
-        // WHEN
-        boolean actual = PasswordChecker.checkPasswordUpperAndLowerCase(passwrod);
-        // THEN
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "ASDASGAQW"
+            }
+    )
+    void checkPasswordUpperAndLowerCase_shouldReturnFalse_whenPasswordDoesntContainOnlyUpperCaseLetters(String password) {
+        boolean actual = PasswordChecker.checkPasswordUpperAndLowerCase(password);
         Assertions.assertFalse(actual);
     }
 
-    @Test
-    void checkPasswordUpperAndLowerCase_shouldReturnFalse_whenPasswordDoesntContainOnlyLowerCaseLetters() {
-        // GIVEN
-        String passwrod = "asdfsadf";
-        // WHEN
-        boolean actual = PasswordChecker.checkPasswordUpperAndLowerCase(passwrod);
-        // THEN
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "asdfsadf"
+            }
+    )
+    void checkPasswordUpperAndLowerCase_shouldReturnFalse_whenPasswordDoesntContainOnlyLowerCaseLetters(String password) {
+        boolean actual = PasswordChecker.checkPasswordUpperAndLowerCase(password);
         Assertions.assertFalse(actual);
     }
 
-    @Test
-    void checkPasswordIfWeak_shouldReturnFalse_whenPasswordIsCommon() {
-        // GIVEN
-        String[] passwords = {"Password1", "Aa345678"};
-        // WHEN
-        boolean actual = false;
-        for (String password : passwords) {
-            actual = PasswordChecker.checkPasswordIfWeak(password);
+    @ParameterizedTest
+    @CsvSource(
+        delimiter = '=',
+        value = {
+                "Password1",
+                "Aa345678"
         }
-        // THEN
+    )
+    void checkPasswordIfWeak_shouldReturnFalse_whenPasswordIsCommon(String passwords) {
+        boolean actual = PasswordChecker.checkPasswordIfWeak(passwords);
         Assertions.assertFalse(actual);
     }
 
-    @Test
-    void checkPasswordSpecialCharacters_shouldReturnTrue_whenPasswordContainsSpecialCharacters() {
-        // GIVEN
-        String password = "/adsf";
-        // WHEN
+    @ParameterizedTest
+    @CsvSource(
+        value = {
+                "/adsf"
+        }
+    )
+    void checkPasswordSpecialCharacters_shouldReturnTrue_whenPasswordContainsSpecialCharacters(String password) {
         boolean actual = PasswordChecker.checkPasswordSpecialCharacters(password);
-        // THEN
         Assertions.assertTrue(actual);
     }
 }
